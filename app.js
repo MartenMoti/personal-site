009ce5defc201d61c7258cef4f0b2fc5f59b4cae
Marten Voorberg <martenvoorberg@gmail.com>
2018-12-16 13:44:13 +0100

    Fix git issues

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let express = require('express');
 let app = express();
 const bodyParser = require('body-parser');
@@ -57,63 +56,3 @@ app.get('/portofolio', function(req, res) {
 });
 
 app.listen(3000);
-=======
-var express = require('express');
-var app = express();
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-
-app.use(express.static(__dirname + '/static'));
-
-app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-
-app.get('/', function(req, res) {
-    res.render('index.ejs', {});
-});
-
-app.get('/contact', function(req, res) {
-    res.render('contact.ejs', {});
-});
-
-app.post('/sendcontact', function(req, res) {
-    let name = req.body.name;
-    let email = req.body.email;
-    let message = req.body.message;
-
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'mailervoorberg@gmail.com', 
-            pass: 'saywhatagain'
-        }
-    });
-
-    let mailOptions = {
-        from: 'mailervoorberg@gmail.com',
-        to: 'martenvoorberg@gmail.com',
-        subject: 'Message from ' + name,
-        text: message
-    };
-
-    transporter.sendMail(mailOptions, function(err, info) {
-        if (error) {
-            res.status(500).send('An error occured whilst sending the email');
-            console.log(error);
-        }
-    });
-
-    res.render('sendcontact.ejs');
-});
-
-app.get('/about', function(req ,res) {
-    res.render('about.ejs');
-});
-
-app.get('/portofolio', function(req, res) {
-    res.render('portofolio.ejs');
-});
-
-app.listen(3000);
->>>>>>> 56d6a0cdcbb8f210d71eccee711cfb6036f1db79
